e6be11126d0891faae270591c0d80c0a067ee1bb
LutzKuenneke <lutz.kuenneke89@gmail.com>
2018-10-19 06:55:18 +0200

    Added keras digit recon

---
+++ digit-recognizer/digits_keras.py
@@ -0,0 +1,33 @@
+#
+# Keras model to compute the digit recognition example from kaggle
+#
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+import pandas as pd
+import numpy as np
+# prepare keras
+model = Sequential([
+    Dense(32, input_shape=(784,)),
+    Activation('relu'),
+    Dense(10),
+    Activation('softmax'),
+])
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+# read the data
+df_train = pd.read_csv('train.csv')
+y = np.array(df_train['label'].values)
+x = np.array(df_train.drop('label', axis = 1))
+# do the thing
+model.fit(x, y, epochs=10)#, batch_size=32)
+
+x_pred = np.array(pd.read_csv('test.csv'))
+y_pred = model.predict(x_pred.values)
+id_axis = range(1,x_pred.shape[0]+1)
+subfile = open('submission_keras.csv', 'w')
+subfile.write('ImageId,Label\n')
+for _id, label in zip(id_axis, y_pred):
+ subfile.write(str(int(_id)) + ',' + str(int(label)) + '\n')
+subfile.close()
